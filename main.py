23273e63a32112b9349183e926964ca0accfe4e6
Your Name <you@example.com>
2026-01-22 10:40:09 +0500

    FastAPI first API

--- main.py
+++ main.py
@@ -64,3 +64,16 @@ def delete_user(user_id: int):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    
+@app.post("/items/")
+def create_item(item: Item):  # Pydantic модель
+    return item
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id, "name": "Влад"}
+import os
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
